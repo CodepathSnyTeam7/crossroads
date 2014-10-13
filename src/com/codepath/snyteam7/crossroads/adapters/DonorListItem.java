83f35e462dbdf4a0b83158277792f82e3ca9f429
Naveen Kumar <n102612@gmail.com>
2014-10-13 14:28:44 -0700

    Commiting Donor Home activity basic skeleton code

---
+++ src/com/codepath/snyteam7/crossroads/adapters/DonorListItem.java
@@ -0,0 +1,8 @@
+package com.codepath.snyteam7.crossroads.adapters;
+
+public class DonorListItem {
+	public String itemName;
+	public String imgUrl;
+	public String desc;
+	public long createTime;
+}
